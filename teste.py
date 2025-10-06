df072caa7fed4f9cd2427e0adfe6428e91f98a37
Lara Dutra <laaahdutra@gmail.com>
2025-10-06 18:05:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,11 +7,4 @@ st.write("Agora o Colab é passado, venha para o Streamlit")
 nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write (nome.upper())
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
-# Caminho da imagem (no mesmo diretório do arquivo .py)
-img = mpimg.imread('imagem programação.jpg')
-plt.imshow(img)
-plt.axis('off')  # Oculta os eixos
-plt.show()
