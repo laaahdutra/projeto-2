cf2e56be114544738cb1b325c7e9d1e2b8bbd43b
Lara Dutra <laaahdutra@gmail.com>
2025-10-06 15:20:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+st.title("Meu programa")
 st.write("Alô mundo") 
+st.write("Agora o Colab é passado, venha para o Streamlit")
+
+st.text_imput("Digite o seu nome:")
+if nome:
+    st.write(nome.upper())
