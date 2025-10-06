dfaab6f39b33693ebccabd31f4abe416014fe0ba
Lara Dutra <laaahdutra@gmail.com>
2025-10-06 18:49:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title("Meu programa")
 st.write("Alô mundo") 
-st.write("Agora o Colab é passado, venha para o Streamlit")
+st.write("Agora o Colab é passado, venha para o Github e veja o resultado no Streamlit")
 
 nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write (nome.upper())
 st.image("programacaoo.jpg")
-st.write("Eu na aula de hoje programando")
+st.write("Eu na aula de hoje programando :)")
 st.image("meninadandooiprog.jpg")
 st.write("Oiii, Josir!! Bem vindo!")
