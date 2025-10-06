8c1fefc8c65ddd2329d9ca39dc33716f589b646f
Lara Dutra <laaahdutra@gmail.com>
2025-10-06 18:46:20 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ st.write("Agora o Colab é passado, venha para o Streamlit")
 nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write (nome.upper())
-    st.image("programacaoo.jpg"),
+st.image("programacaoo.jpg")
 st.write("Eu na aula de hoje programando")
 st.image("meninadandooiprog.jpg")
 st.write("Oiii, Josir!! Bem vindo!")
