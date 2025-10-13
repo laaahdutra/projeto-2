b853e0397f40ecfa6769f73060e28fbec44a0dad
Lara Dutra <laaahdutra@gmail.com>
2025-10-13 15:12:11 -0300

    paises.py

--- paises.py
+++ paises.py
@@ -13,4 +13,5 @@ fig = px.scatter_geo(dataset,
                      lon=dataset['longitude'],
                      hover_name=dataset['nome'])
 fig.update_layout(title= 'Coordenadas dos Países do Mapa', geo_scope= 'world')
+
 st.plotly_chart(fig,  use_container_width=True, theme="sreamlit") 
