07c5645ed097158cab4a9224ac3f373ae2a2a07e
Lara Dutra <laaahdutra@gmail.com>
2025-10-13 15:14:10 -0300

    paises.py

--- paises.py
+++ paises.py
@@ -5,6 +5,8 @@ import plotly.express as px
 st.title("Países")
 dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
+st.dataframe(dataset)
+
 fig = px.scatter_geo(dataset,
                      lat=dataset['latitude'],
                      lon=dataset['longitude'],
