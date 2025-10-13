4267586c230b905a3077572145bdbc9f11d67d78
Lara Dutra <laaahdutra@gmail.com>
2025-10-13 15:20:17 -0300

    paises.py

--- paises.py
+++ paises.py
@@ -7,10 +7,10 @@ dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
 st.dataframe(dataset)
 
-fig = px.scatter_geo(dataset,
-                     lat=dataset['latitude'],
-                     lon=dataset['longitude'],
-                     hover_name=dataset['nome'])
-fig.update_layout(title= 'Coordenadas dos Países do Mapa', geo_scope= 'world')
+fig = px.choropleth(dataset,
+                    locations=dataset['iso3'],
+                    color=dataset['nome'],
+                    hover_name=dataset['nome'])
+fig.update_layout(title="Mapa Coroplético dos Países", geo_scope='world')
 
 st.plotly_chart(fig,  use_container_width=True, theme="streamlit") 
